d87feeb5319b1d7a9a761729634dd31305754bbf
Anand Biehl <36610944+biehla@users.noreply.github.com>
2025-02-21 12:57:01 -0500

    Logout is now visually confirmed

--- src/lib/login-outButton.svelte
+++ src/lib/login-outButton.svelte
@@ -2,4 +2,8 @@
 	let { text = 'Sign In', href = '/login' } = $props();
 </script>
 
-<a {href} class="text-base font-medium text-gray-700 hover:text-gray-900">{text}</a>
+{#if href === '/login'}
+	<a {href} class="text-base font-medium text-gray-700 hover:text-gray-900">{text}</a>
+{:else}
+	<a {href} class="text-base font-medium text-gray-700 hover:text-gray-900" data-sveltekit-reload>{text}</a>
+{/if}
--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,10 +1,9 @@
 import { invalidateSession, deleteSessionTokenCookie } from "$lib/server/session";
 import {  redirect } from "@sveltejs/kit";
-import type { Handle } from "@sveltejs/kit"
+import type { ServerLoad } from "@sveltejs/kit"
+// import {goto} from "$app/navigation"
 
-export const load: Handle = async ({ event, resolve }) => {
-  console.log("We logging out!", event)
-
+export const load: ServerLoad = async (event) => {
   if (!event) {
     redirect(302, "/")
   }
@@ -16,20 +15,17 @@ export const load: Handle = async ({ event, resolve }) => {
   if (!event.locals.user) {
     redirect(302, "/")
   }
+
   if (!event.locals.session) {
     redirect(302, "/")
   }
 
-  console.log("We logging out!")
   try {
-    console.log("Invalidating session")
     await invalidateSession(event.locals.session.id);
   }
   catch (e) {
-    console.log("some error from logout: ", e);
     redirect(505, "/")
   }
-  console.log("deleting cookies")
   deleteSessionTokenCookie(event);
-  redirect(302, "/");
+  redirect(302, "/")
 }
