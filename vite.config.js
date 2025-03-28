651834e672af481e0327679bc3bc8b8d9ab28e9a
Anand Biehl <36610944+biehla@users.noreply.github.com>
2025-03-27 23:43:01 -0400

    feat: Enable SSR for client-only module 'leaflet' in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit(), tailwindcss()],
+  ssr: {
+    noExternal: ['leaflet']
+  },
   server: {
     watch: {
       // useFsEvents: true, // macOS only
